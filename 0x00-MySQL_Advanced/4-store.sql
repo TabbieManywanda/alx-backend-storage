d1684c8702a0648f31962d57581acd255e81f31c
TabbieManywanda <achiengtabitha@gmail.com>
2023-04-06 19:29:18 +0000

    Edit

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,7 +2,7 @@
 
 CREATE TRIGGER decrease_quantity
 AFTER INSERT ON orders
-FOR EACH ROW UPDATE ITEMS
-SET
-quantity = quantity - NEW.number
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
 WHERE name = NEW.item_name;
