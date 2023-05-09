1a7e51210666cfaf5e761b3e0ecd1f908df35954
TabbieManywanda <achiengtabitha@gmail.com>
2023-05-09 11:51:54 +0000

    Edit - Pycodestyle whitespace edit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,7 +10,7 @@ from functools import wraps
 
 def count_calls(method: Callable) -> Callable:
     ''' define count calls '''
-    
+
     @wraps(method)
     def wrapper(self, *args, **kwds):
         ''' define wrapper '''
