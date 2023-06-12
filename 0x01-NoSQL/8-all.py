2b629e267ef1795b0431e2e1967f1e75c07b444a
TabbieManywanda <achiengtabitha@gmail.com>
2023-06-12 07:03:11 -0700

    function that lists all documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#/usr/bin/env python3
+
+'''lists all documents in a collection'''
+
+def list_all(mongo_collection):
+    '''Return an empty list if no
+    document in the collection'''
+
+    documents = mongo_collection.find()
+    return documents
