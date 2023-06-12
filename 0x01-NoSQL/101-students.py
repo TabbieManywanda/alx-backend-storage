e018694398364f8f7b80f35f452dff128f6f0898
TabbieManywanda <achiengtabitha@gmail.com>
2023-06-12 07:28:55 -0700

    Python function that returns all students sorted by average score

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+''' returns all students sorted by average score'''
+
+def top_students(mongo_collection):
+    '''The top must be ordered
+    The average score must be part of each item
+    returns with key = averageScore'''
+
